78592b06937ef662c2904d549098d09bf699807d
Vipul <vipulpandey7047@gmail.com>
2026-03-25 23:03:55 +0530

    initial commit

---
+++ java32.java
@@ -0,0 +1,14 @@
+//Java program to find the duplicate elements in the array using index
+
+public class java32 {
+    public static void main(String[] args) {
+        int[] a = { 3, 4, 5, 3, 6, 4 };
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] == a[j]) {
+                    System.out.println(a[i] + " is duplicated in the array");
+                }
+            }
+        }
+    }
+}
